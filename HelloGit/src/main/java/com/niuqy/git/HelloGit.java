4b9695d030763e78e99f5a9c41bbd5def4efa65a
niuqy_glb <iniuqingyi@163.com>
2020-10-24 23:59:00 +0800

    [dev-2分支]update HelloGit

--- HelloGit/src/main/java/com/niuqy/git/HelloGit.java
+++ HelloGit/src/main/java/com/niuqy/git/HelloGit.java
@@ -8,5 +8,8 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("dev分支添加");
+        System.out.println("dev2分支-11行添加");
+        System.out.println("dev2分支-12行添加");
+        System.out.println("dev2分支-13行添加");
     }
 }
